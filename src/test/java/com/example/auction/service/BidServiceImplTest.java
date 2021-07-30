4bbc658ebe43b48264de11493018487e4f591231
mikeyjay <mjeszenka@trackx.com>
2021-07-30 09:01:33 -0700

    Add unit tests

--- src/test/java/com/example/auction/service/BidServiceImplTest.java
+++ src/test/java/com/example/auction/service/BidServiceImplTest.java
@@ -7,10 +7,10 @@ import com.example.auction.domain.User;
 import com.example.auction.dto.ApiResponse;
 import com.example.auction.dto.AuctionItemDto;
 import com.example.auction.dto.PostBidsRequest;
+import com.example.auction.exception.PostBidsException;
 import com.example.auction.repository.AuctionItemRepository;
 import com.example.auction.repository.UserRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -33,6 +33,8 @@ class BidServiceImplTest {
 	User selfUser;
 	User user;
 	AuctionItem auctionItem;
+	BigDecimal previousCurrentBid;
+	BigDecimal previousMaxAutoBid;
 
 	@BeforeEach
 	void setUp() {
@@ -63,11 +65,32 @@ class BidServiceImplTest {
 		Mockito.doReturn(selfUser)
 				.when(userRepository)
 				.findByUsername(Mockito.anyString());
+
+		previousCurrentBid = new BigDecimal(auctionItem.getCurrentBid().toString());
+		previousMaxAutoBid = new BigDecimal(auctionItem.getMaxAutoBidAmount().toString());
 	}
 
 	@Test
 	void postBidsOutbidded() throws Exception {
-		BigDecimal previousCurrentBid = new BigDecimal(auctionItem.getCurrentBid().toString());
+		postBidsOutbiddedTest();
+	}
+
+	@Test
+	void postBidsNewHighBid() throws Exception {
+		postBidsNewHighBidTest();
+	}
+
+	@Test
+	void postBidsInvalidBidMet() throws Exception {
+		postBidsInvalidBidTest();
+	}
+
+	@Test
+	void postBidsReservePriceNotMet() throws Exception {
+		postBidsReservePriceNotMetTest();
+	}
+
+	private void postBidsOutbiddedTest() throws Exception {
 		PostBidsRequest postBidsRequest = new PostBidsRequest();
 		postBidsRequest.setAuctionItemId(auctionItem.getId().toString());
 		postBidsRequest.setBidderName(selfUser.getUsername());
@@ -78,5 +101,41 @@ class BidServiceImplTest {
 				user.getUsername(),
 				response.getResult().getBidderName());
 		Assert.assertNotEquals("Current bid did not change", previousCurrentBid, response.getResult().getCurrentBid());
+	}
+
+	private void postBidsNewHighBidTest() throws Exception {
+		PostBidsRequest postBidsRequest = new PostBidsRequest();
+		postBidsRequest.setAuctionItemId(auctionItem.getId().toString());
+		postBidsRequest.setBidderName(selfUser.getUsername());
+		postBidsRequest.setMaxAutoBidAmount(previousMaxAutoBid.add(previousMaxAutoBid));
+		ApiResponse<AuctionItemDto> response = bidService.postBids(postBidsRequest);
+		Assert.assertEquals("Status should be SUCCESS", ApiStatus.SUCCESS.toString(), response.getStatus());
+		Assert.assertEquals("Current bidder should have changed",
+				selfUser.getUsername(),
+				response.getResult().getBidderName());
+		Assert.assertEquals("Current bid should be 1 higher than previous max bid",
+				previousMaxAutoBid.add(BigDecimal.ONE), response.getResult().getCurrentBid());
+		Assert.assertEquals("Max auto bid not correctly set",
+				previousMaxAutoBid.add(previousMaxAutoBid),
+				response.getResult().getMaxAutoBidAmount());
+	}
+
+	private void postBidsInvalidBidTest() throws Exception {
+		PostBidsRequest postBidsRequest = new PostBidsRequest();
+		postBidsRequest.setAuctionItemId(auctionItem.getId().toString());
+		postBidsRequest.setBidderName(selfUser.getUsername());
+		postBidsRequest.setMaxAutoBidAmount(BigDecimal.ZERO.subtract(BigDecimal.ONE));
+		Assert.assertThrows(PostBidsException.class, () -> bidService.postBids(postBidsRequest));
+	}
+
+	private void postBidsReservePriceNotMetTest() throws Exception {
+		PostBidsRequest postBidsRequest = new PostBidsRequest();
+		postBidsRequest.setAuctionItemId(auctionItem.getId().toString());
+		postBidsRequest.setBidderName(selfUser.getUsername());
+		postBidsRequest.setMaxAutoBidAmount(BigDecimal.ONE);
+		ApiResponse<AuctionItemDto> response = bidService.postBids(postBidsRequest);
+		Assert.assertEquals("Should return reserve price not met status",
+				"Bid has not met the reserve price",
+				response.getStatus());
 	}
 }

366c2adbb7b1b84d3b1fb5f13d74381f3d83bd8e
mikeyjay <mjeszenka@trackx.com>
2021-07-31 14:42:34 -0700

    Save progress

--- frontend/auction/src/app/app.component.ts
+++ frontend/auction/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +10,49 @@ import { catchError, retry } from 'rxjs/operators';
 export class AppComponent {
   title = 'auction';
   auctionItems: any = [];
-  constructor(private http: HttpClient) { }
+  auctionItem: AuctionItemResponse = <AuctionItemResponse>{};
+
+  constructor(private http: HttpClient) {
+  }
 
   getAllAuctionItems() {
+    this.auctionItem = <AuctionItemResponse>{};
     this.http.get("http://localhost:8080/api/v1/auctionItems").subscribe(
       (response) => {
         this.auctionItems = response;
       }
     );
   }
+
+  getAuctionItems(id: string) {
+    this.auctionItems = null;
+    let requestUrl: String;
+    requestUrl = "http://localhost:8080/api/v1/auctionItems/".concat(id);
+    console.log(requestUrl);
+
+    this.http.get(requestUrl.toString()).subscribe(
+      (response) => {
+        this.auctionItem = (<AuctionItemResponse>response);
+      }
+    );
+  }
+}
+
+interface AuctionItemResponse {
+  status: string
+  result: AuctionItem
+}
+
+interface AuctionItem {
+  auctionItemId: string
+  reservePrice: number
+  currentBid: number
+  maxAutoBidAmount: number
+  item: Item
+  bidderName: string
+}
+
+interface Item {
+  itemId: string
+  description: string
 }
--- frontend/auction/src/app/app.module.ts
+++ frontend/auction/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuctionComponent } from './auction/auction.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AuctionComponent } from './auction/auction.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

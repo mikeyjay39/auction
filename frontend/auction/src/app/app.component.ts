80b29683ee9cf2c842b9fa66e048b69080cd6d53
mikeyjay <mjeszenka@trackx.com>
2021-07-31 13:03:12 -0700

    Add getAllAuctionItems to frontend

--- frontend/auction/src/app/app.component.ts
+++ frontend/auction/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
   title = 'auction';
+  auctionItems: any = [];
+  constructor(private http: HttpClient) { }
+
+  getAllAuctionItems() {
+    this.http.get("http://localhost:8080/api/v1/auctionItems").subscribe(
+      (response) => {
+        this.auctionItems = response;
+      }
+    );
+  }
 }

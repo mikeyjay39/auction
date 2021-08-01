dbe336756a18525e6e0af3cc6120fac842c1aedb
mikeyjay <mjeszenka@trackx.com>
2021-08-01 10:14:55 -0700

    Fix caching

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,8 @@
-insert into item (id, description) values (1, 'guitar');
-insert into item (id, description) values (2, 'microphone');
-insert into item (id, description) values (3, 'drums');
+insert into item (description) values ('guitar');
+insert into item (description) values ('microphone');
+insert into item (description) values ('drums');
+insert into item (description) values ('amplifier');
 insert into app_user (username, password) values ('John Lennon', 'password');
+insert into app_user (username, password) values ('Paul McCartney', 'password');
+insert into app_user (username, password) values ('George Harrison', 'password');
+insert into app_user (username, password) values ('Ringo Starr', 'password');
